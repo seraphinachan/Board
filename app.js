9df4c41c809ac7d02f4b9e0229b51b9ab5a39a46
Administrator <jiyeonyee0312@gmail.com>
2023-11-08 22:41:41 +0900

    message

--- app.js
+++ app.js
@@ -11,8 +11,37 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.get('/', function (req, res) {
-    res.render('index.ejs')
+    res.render('index')
 });
+
+// POST 로 값을 전송할 때 HTTP 패킷의 Request Body 영역의 값을 가져와야 합니다.
+// 그러기 위해서는 Express 내장 함수를 사용하거나 외부 Body-parser 를 사용해야 합니다.
+app.use(express.urlencoded({extended: true}));
+
+// router
+var indexRouter = require('./router/');
+var resultRouter = require('./router/result');
+
+app.use('/', indexRouter);
+app.use('/', resultRouter);
+
+//jquery
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // jquery JS
+
+var uploadRouter = require('./router/upload');
+app.use('/', uploadRouter);
+
+// app.get('/result', function (req, res) {
+//     var val1 = req.query.val1;
+//     var val2 = req.query.val2;
+//     res.render('result', {'val1': val1, 'val2':val2});
+// })
+
+// app.post('/result', function (req, res) {
+//     val1 = req.body.val1;
+//     val2 = req.body.val2;
+//     res.render('result', {'val1': val1, 'val2': val2});
+// })
 
 app.listen(3000, function() {
     console.log("3000 포트로 노드 서버 오픈!");
