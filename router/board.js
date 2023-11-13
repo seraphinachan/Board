df77272f90ec7ff34c95268fd3a55fdd4f4c4870
awesomeji <thswl1515@gmail.com>
2023-11-13 15:19:05 +0900

    message

--- router/board.js
+++ router/board.js
@@ -1,33 +1,45 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../lib/db');
+var db = require("../lib/db");
 
-router.use(express.urlencoded({
-    extended: true
-}));
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-router.get('/board/write', function (req, res) {
-    res.render('board/write');
+router.get("/board/write", function (req, res) {
+  res.render("board/write");
 });
 
-router.get('/board', function (req, res) {
-    var sql = "select * from board";
-    db.query(sql, function (err, rows) {
-        res.render('board', {
-            rows: rows
-        })
-    })
-})
+router.get("/board", function (req, res) {
+  var sql = "select * from board";
+  db.query(sql, function (err, rows) {
+    res.render("board", {
+      rows: rows,
+    });
+  });
+});
 
-router.post('/board/write', function (req, res) {
-    var writer = req.body.writer;
-    var password = req.body.password;
-    var title = req.body.title;
-    var content = req.body.content;
-    var data = [writer, password, title, content];
-    var sql = "insert into board(idx, writer, password, title, content, date, del_yn) values(null,?,?,?,?,now(),'N')";
-    db.query(sql, data);
-    res.redirect('/board')
-})
+router.get("/board/view/:idx", function (req, res) {
+  var idx = req.params.idx;
+  var sql = "select * from board where 1=1 and idx=?";
+  db.query(sql, [idx], function (err, rows) {
+    res.render("board/view", {
+      result: rows[0],
+    });
+  });
+});
+
+router.post("/board/write", function (req, res) {
+  var writer = req.body.writer;
+  var password = req.body.password;
+  var title = req.body.title;
+  var content = req.body.content;
+  var data = [writer, password, title, content];
+  var sql = "insert into board(idx, writer, password, title, content, date, del_yn) values(null,?,?,?,?,now(),'N')";
+  db.query(sql, data);
+  res.redirect("/board");
+});
 
 module.exports = router;
